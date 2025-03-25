a7ee23a8ecc1dde78db0171a9877d9175f947eea
Dennis <u17305518@utp.edu.pe>
2025-03-24 23:23:20 -0500

    Primer Commit - Proyecto Lading Page With Login

---
+++ js/login.js
@@ -0,0 +1,12 @@
+function login() {
+    
+    let user=document.getElementById("username").value;
+    let password=document.getElementById("password").value;
+
+    if (user=="dennis" && password=="123456") {
+        window.location.href = "../view/home.html"; 
+       
+    }else{
+        alert("Datos Incorrectos")
+    }
+}
