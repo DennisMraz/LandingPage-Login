52d1df0e15e0ff39eb7a1e3fa4a419f26a61da33
Dennis Rojas <75002123+DennisMraz@users.noreply.github.com>
2025-03-24 23:42:02 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -4,7 +4,9 @@ function login() {
     let password=document.getElementById("password").value;
 
     if (user=="dennis" && password=="123456") {
-        window.location.href = "/LandingPage-Login/view/home.html";
+        const baseUrl = new URL(window.location.origin + "/LandingPage-Login/");
+        const homeUrl = new URL("view/home.html", baseUrl);
+        window.location.href = homeUrl.href;
        
     }else{
         alert("Datos Incorrectos")
